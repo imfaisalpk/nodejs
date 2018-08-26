6c7273704c231290ca1ca8e8862977c5fdf0af23
unknown <muhammadfaisal9t3@outlook.com>
2018-08-26 17:08:55 +0500

    Import Logger Module

---
+++ app.js
@@ -0,0 +1,16 @@
+const logger = require('./logger');
+
+// console.log(logger);
+// logger.login("MFAISAL");
+logger("MFAISAL");
+
+
+
+// function hello(name){
+//     console.log("Hello "+name);
+// }
+
+// hello("Pakistan!");
+// console.log(window);
+
+// console.log(module);
---
+++ logger.js
@@ -0,0 +1,18 @@
+// console.log(exports);
+// console.log(require);
+// console.log(module);
+// console.log(__filename);
+// console.log(__dirname);
+
+
+// console.log("---Logger---");
+var url = "https://imfaisal.com";
+
+function login(input){
+    // Send login info.
+    console.log(input);
+}
+
+// module.exports.login = login;
+module.exports = login;
+
